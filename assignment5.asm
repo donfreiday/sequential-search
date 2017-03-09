e8090cea609e171ab28f1833f2a5e69768755328
don <donfreiday@email.com>
2017-03-09 10:18:29 -0500

    More cleanup

--- assignment5.asm
+++ assignment5.asm
@@ -1,11 +1,8 @@
+;-----------------------------------------------------------------------------------------------------
 ; assignment5.asm - CISC225 Assignment 5 (Sequential Search)
 ; Don Freiday
 ; 3/7/2017
-
-; Also include a test program that demonstrates that the function works. The test program should output
-; all values in the test array, prompt for a target value, then display either the array position in which the
-; target value was found, or a message saying that the value was not found
-
+;-----------------------------------------------------------------------------------------------------
 
 INCLUDE Irvine32.inc
 
@@ -32,24 +29,24 @@ main proc
 
 	call Search
 
-	jnz notfound ; Print message if value is not found, otherwise print index at which it was found
+	; Print message if value is not found, otherwise print index at which it was found
+	jnz notfound 
 	mov edx,offset valFound
 	call WriteString
 	mov eax,ecx ; Index of found value should be in ecx; move to eax for WriteInt procedure
 	call WriteInt
-	jmp done
+	jmp quit
 notfound:
 	mov edx,offset valNotFound
 	call WriteString
-done:
+quit:
 	call CrLf
 	call CrLf
 	call WaitMsg
-
 	invoke ExitProcess,0 
 main endp
 
-
+;-----------------------------------------------------------------------------------------------------
 ; Search:  procedure to carry out a sequential search on a double-word array.
 ; Expects:
 ;	ESI – The base address of the array to be search.
@@ -62,26 +59,20 @@ main endp
 ;	ECX – The position (array-based index) in which the target value was found. If the target value
 ;		was not found, return the original length of the array that was given.
 ;	ZF - If the target was found, Zero Flag will be set. If not found, the Zero Flag will be clear.
-Search proc
-	; Preserve registers
-	push eax
-	push esi
-	push ecx
-
+;-----------------------------------------------------------------------------------------------------
+Search proc uses eax esi
+	push ecx ; Preserve length of the array
 L1:
 	; Loop through array, checking each index position for desired value
 	cmp eax,[esi] ; Compare target value to value at current address in array
 	je found ; If value is found, handle it
 	add esi,type array ; Increment address in esi by the size of one array element
-	loop L1
-	
+	loop L1	
 notfound:
 	; Clear ZF, return length of array in ecx. Label is for clarity, not neccessary.
 	inc ecx ; Clear the zero flag, ecx was just used as counter for the loop instruction and should be >= 0
 	pop ecx ; Return the length of the array that was given initially in ecx
-	jmp preserve 
-	
-	
+	ret ; 'Uses' directive will cause the assembler to insert pop eax and pop esi before ret, preserving their values
 found: 
 	; Return address of found value in edi, index in ecx, and set ZF
 	mov edi,esi ; Move address of found value to edi
@@ -89,38 +80,26 @@ found:
 	sub eax,ecx ; Calculate index of found value
 	mov ecx,eax ; Move index of found value into ecx
 	xor eax,eax ; Set zero flag
-
-preserve: ; Preserve registers
-	pop esi
-	pop eax
-
-	ret
+	ret ; 'Uses' directive will cause the assembler to insert pop eax and pop esi before ret, preserving their values
 Search endp
 
-
+;-----------------------------------------------------------------------------------------------------
 ; PrintArray: Prints an array of integers
 ; Expects:
 ;	ESI – The base address of the array
 ;	ECX – The length of the array
 ; Returns:
 ;	Nothing
-PrintArray proc
-	push eax
-	push esi
-	push ecx
+;-----------------------------------------------------------------------------------------------------
+PrintArray proc uses eax esi ecx
 L1:
-	mov eax,' '
+	mov eax,[esi] ; Move the value at address contained in esi to eax
+	call WriteInt ; Display the value in eax
+	mov eax,' ' ; Separate each value displayed with a ' ' character
 	call WriteChar
-	mov eax,[esi]
-	call WriteInt
-	add esi,type array
+	add esi,type array ; Point esi at the next array element's address
 	loop L1
-	
 	call CrLf
-
-	pop ecx
-	pop esi
-	pop eax
 	ret
 PrintArray endp
 
